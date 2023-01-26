daf7ebca02fbffafc760c600db2adc09fb29bd4a
gamerslink <100190888+gamerslink@users.noreply.github.com>
2023-01-26 22:59:12 +0800

    商品功能 - 實作查詢商品列表 (排序)

--- src/main/java/com/jackyliao/springbootmall/controller/ProductController.java
+++ src/main/java/com/jackyliao/springbootmall/controller/ProductController.java
@@ -21,13 +21,20 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts(
+            // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category, // required = false 非必填
-            @RequestParam(required = false) String search
+            @RequestParam(required = false) String search,
+
+            // 排序 Sorting
+            @RequestParam(defaultValue = "created_date") String orderBy,
+            @RequestParam(defaultValue = "desc") String sort
     ) {
 
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
+        productQueryParams.setOrderBy(orderBy);
+        productQueryParams.setSort(sort);
 
        List<Product> productList = productService.getProducts(productQueryParams);
 
