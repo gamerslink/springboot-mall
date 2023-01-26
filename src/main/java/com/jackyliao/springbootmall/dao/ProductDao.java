f4ab03d055c2549b6abbedd0c9a322070d354e6b
gamerslink <100190888+gamerslink@users.noreply.github.com>
2023-01-26 21:09:38 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- src/main/java/com/jackyliao/springbootmall/dao/ProductDao.java
+++ src/main/java/com/jackyliao/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.jackyliao.springbootmall.dao;
 import com.jackyliao.springbootmall.dto.ProductRequest;
 import com.jackyliao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/jackyliao/springbootmall/service/ProductService.java
+++ src/main/java/com/jackyliao/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.jackyliao.springbootmall.service;
 import com.jackyliao.springbootmall.dto.ProductRequest;
 import com.jackyliao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
