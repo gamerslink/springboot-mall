46bfa523661a09ad3e6afaeb9cf2a00cbdaef66d
gamerslink <100190888+gamerslink@users.noreply.github.com>
2023-01-26 23:59:31 +0800

    商品功能 - 實作查詢商品列表 (分頁)

--- src/main/java/com/jackyliao/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/jackyliao/springbootmall/dao/impl/ProductDaoImpl.java
@@ -112,6 +112,7 @@ public class ProductDaoImpl implements ProductDao {
 
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢條件
         if (productQueryParams.getCategory() != null) {
             sql = sql + " AND category = :category";
             map.put("category", productQueryParams.getCategory().name()); // 前端傳來的值 category，name()方法 enum 轉換成字串，字串值在加到 map
@@ -122,7 +123,13 @@ public class ProductDaoImpl implements ProductDao {
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
+        // 排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        // 分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
