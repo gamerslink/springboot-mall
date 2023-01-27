f942adb063e77dfdfabcada589782598c2d48e4f
gamerslink <100190888+gamerslink@users.noreply.github.com>
2023-01-27 17:26:07 +0800

    帳號功能 - 單元測試

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -10,3 +10,16 @@ CREATE TABLE IF NOT EXISTS product
     created_date       TIMESTAMP    NOT NULL,
     last_modified_date TIMESTAMP    NOT NULL
     );
+
+
+
+-- H2 資料庫廠商不同，UNIQUE 語法不同
+CREATE TABLE IF NOT EXISTS user
+(
+    user_id            INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email              VARCHAR(256) NOT NULL UNIQUE,
+    password           VARCHAR(256) NOT NULL,
+    created_date       TIMESTAMP    NOT NULL,
+    last_modified_date TIMESTAMP    NOT NULL
+    );
+
